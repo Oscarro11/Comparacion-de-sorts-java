d57c0ff2c7e26fe86f36d9a53d1ec61fc607552f
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-02-08 22:27:44 -0600

    feat: Controller class created

---
+++ src/main/java/Controlador.java
@@ -0,0 +1,63 @@
+package main.java;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Collections;
+import java.time.*;
+
+public class Controlador {
+    private List<Integer> numerosOrdenar;
+
+    public void generarListaDesordenada(int tamano) throws IOException{
+        try {
+            GeneradorNumeros.crearArchivoAleatorio(tamano);
+            numerosOrdenar = LectorNumeros.obtenerNumeros();    
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    public void generarListaOrdenada(int tamano) throws IOException{
+        try {
+            generarListaDesordenada(tamano);    
+        } catch (IOException e) {
+            throw e;
+        }
+
+        Collections.sort(numerosOrdenar);
+    }
+
+    public String generarReporte() throws NullPointerException{
+
+        if (numerosOrdenar == null) {
+            throw new NullPointerException("Debe generar un conjunto de numeros antes de poder usar el comparador de sorts");
+        }
+
+        ArrayList<ISort<Integer>> sorts = new ArrayList<ISort<Integer>>();
+        StringBuilder builder = new StringBuilder();
+
+        sorts.add(new MergeSort<Integer>());
+        sorts.add(new QuickSort<Integer>());
+        sorts.add(new RadixSort<Integer>());
+
+        for (ISort<Integer> sort : sorts) {
+            builder.append("El tiempo de ejecucion del sort '" + sort.getClass() + "' es: \n");
+            builder.append(calcularTiempoSort(sort) + "\n\n");
+        }
+
+        return builder.toString();
+    }
+
+    private String calcularTiempoSort(ISort<Integer> sort){
+        Instant inicio = Instant.now();
+
+        List<Integer> copiaNumerosOrdenar = new ArrayList<Integer>(numerosOrdenar);
+        sort.ordenar(copiaNumerosOrdenar);
+
+        Instant fin = Instant.now();
+        Duration diferenciaTiempo = Duration.between(inicio, fin);
+
+        return diferenciaTiempo.toString();
+    }
+}
